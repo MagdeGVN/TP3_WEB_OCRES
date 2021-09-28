8a4cb6c88fd88909522d5b02a9b7824df3a9e1c0
Magdeleine <magauvain@hotmail.fr>
2021-09-28 11:20:39 +0200

    Q°3: actualisation de la ville

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -11,7 +11,10 @@ const API_URL_ICON = "http://openweathermap.org/img/wn/";
 class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
+    /*if(city === undefined){
+      city = "paris";
+    }*/
+    if(!city){
       city = "paris";
     }
     this.city = city;
