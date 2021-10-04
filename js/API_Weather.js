6bf8c9ad94180bef2097446e47d30218dd5ebee5
Magdeleine <magauvain@hotmail.fr>
2021-10-04 09:37:11 +0200

    météo sur 4 jours

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -4,6 +4,8 @@
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+// Url API 16 days
+const API_URL_16DAYS = "https://api.openweathermap.org/data/2.5/forecast/daily";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
@@ -31,5 +33,12 @@ class API_WEATHER{
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
+  }
+
+  getThreeDaysForecast(){
+    return axios
+    .get(`${API_URL_16DAYS}?q=${this.city}&units=metric&cnt=5&appid=${API_KEY}`, {
+      crossdomain: true
+    })
   }
 }
--- js/script.js
+++ js/script.js
@@ -6,23 +6,25 @@ function start() {
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
-    .fetchTodayForecast()
+    //.fetchTodayForecast()
+    .getThreeDaysForecast()
     .then(function(response) {
       // Récupère la donnée d'une API
       const data = response.data;
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      for(let i=0; i<4; i++){
+        // On récupère l'information principal
+      const main = data.list[i].weather[0].main;
+      const description = data.list[i].weather[0].description;
+      const temp = data.list[i].temp.day;
+      const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
+        document.getElementById('day' + (i+1) + '-forecast-main').innerHTML = main;
+        document.getElementById('day' + (i+1) + '-forecast-more-info').innerHTML = description;
+        document.getElementById('icon' + (i+1) + '-weather-container').innerHTML = icon;
+        document.getElementById('day' + (i+1) + '-forecast-temp').innerHTML = `${temp}°C`;
+      }
     })
     .catch(function(error) {
       // Affiche une erreur
